eddf9f22e599c6c7974adb8ba2f8c2b783c4641b
turtlegirll <agatapodgorska00@gmail.com>
2026-09-01 23:42:52 +0200

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
+console.log("URL exists:", !!supabaseUrl);
+console.log("Key exists:", !!supabaseAnonKey);
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey,
